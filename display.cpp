87fadf1180627c2ca0266997df87d2e32db5e304
Jagadeep Ganta <152236029+jagadeep017@users.noreply.github.com>
2025-01-18 02:13:55 +0530

    Refactor how the buffer store the data. and how cursor is moving for more efficiency.

--- display.cpp
+++ display.cpp
@@ -1,106 +1,92 @@
 #include "main.h"
 #include "display.h"
-#include <cstring>
+#include <iomanip>
 #include <stdlib.h> 
 #include <iostream>
-#include <fstream>
+
 
 void display(unsigned short int row,unsigned short int col,text& t){
-    t.prev=NULL;
-    t.prev_line_len=0;
+    t.atend=0;
     system("clear");    //clear the screen
-    struct charn *temp=t.head;
+    struct line *temp=t.head;
     unsigned int line_count=0,char_count=0;
     unsigned int courser_line=0,courser_col=0;
-    char flag=1;
-    while (temp!=NULL&&temp->data!=EOF) {
-        if(char_count==t.cursor){
-            t.Cursor=temp;
-            if(temp->data=='\n'){
-                if(line_count==row-3){
-                    t.atend=1;
+    unsigned short temp1=0;
+    unsigned int offset=0;
+    
+    if(t.line_offset+t.cursor_line>=row-4){
+        offset=t.cursor_line+4-row;
+    }
+    line_count=t.line_count;
+    while(line_count){
+        temp1++;
+        line_count/=10;
+    }
+    line_count=offset;
+    while(line_count){
+        temp=temp->next;
+        line_count--;
+    }
+    line_count=offset;
+    while (temp!=NULL) {
+        char_count=0;
+        struct charn *temp2=temp->head;
+        std::cout<<"\033[36m"<<std::setw(temp1)<<line_count+1<<' '<<"\033[0m";
+        while(temp2&&temp2->data!=EOF){
+            if(line_count==t.cursor_line&&char_count==t.cursor_col){
+                if(temp2->data=='\n'){
+                    if(line_count==row-1){
+                        t.atend=1;
+                    }
+                    std::cout<<"\033[47m"<<' '<<"\033[0m"<<temp2->data;
+                    t.prev=temp->prev;
                 }
-                std::cout<<"\033[47m"<<' '<<"\033[0m"<<temp->data;
+                else{
+                    std::cout<<"\033[47m"<<temp2->data<<"\033[0m";
+                }
+                t.Cursor=temp2;
             }
             else{
-                std::cout<<"\033[47m"<<temp->data<<"\033[0m";
+                std::cout<<temp2->data;
             }
-            flag=0;
-            courser_line=line_count;
-        }
-        else{
-            std::cout<<temp->data;
-        }
-        if (temp->data=='\n') {
-            line_count++;
-            if(flag){
-                t.prev_line_len=courser_col;
-                t.prev=temp;
-                courser_col=0;
+            char_count++;
+            temp2=temp2->next;
+            if(char_count+temp1+1==col){
+                std::cout<<std::setw(temp1+1)<<' ';
             }
         }
-        else if(flag){
-            courser_col++;
-        }
-        char_count++;
         temp=temp->next;
-        if(line_count==row-2){
+        line_count++;
+        if(line_count-offset==row-1){
             break;
         }
-        if(courser_col==col){
-            line_count++;
-            courser_col=0;
-        }
     }
-    t.cursor_col=courser_col;
     if(courser_line==line_count-1){
         t.atend++;
     }
-    else{
-        t.atend=0;
+    if(t.line_count<row-1){
+        temp1=row-1-t.line_count;
     }
-    std::cout<<"Ln "<<courser_line+1<<", Col "<<courser_col+1<<std::endl;
-}
-
-void add_data(char* name,struct charn **head,struct charn **tail){
-    char *temp=strstr(name,".");
-    if(temp==NULL){
-        std::cout<<"Invalid file name"<<std::endl;
-        exit(0);
-    }
-    if(strcmp(temp,".txt")!=0 && strcmp(temp,".c")!=0 && strcmp(temp,".cpp")!=0 && strcmp(temp,".h")!=0){
-        std::cout<<"Invalid file type"<<std::endl;
-        exit(0);
-    }
-    std::ifstream file(name);
-    if(!file.is_open()){
-        std::cout<<"File "<<name<<" not found"<<std::endl;
-        exit(0);
-    }
-    while(!file.eof()){
-        char ch=file.get();
-        if(ch!='\r')
-            insert_last(ch,head,tail);
-    }
-    file.close();
-}
-
-void insert_last(char c,struct charn **head,struct charn **tail){
-    struct charn *new1=new charn;
-    if(new1==NULL){
-        std::cout<<"Memory allocation failed"<<std::endl;
-        exit(0);
-    }
-    new1->data=c;
-    new1->next=NULL;
-    new1->prev=NULL;
-    if(*head==NULL){
-        *head=new1;
-        *tail=new1;
+    else if(t.cursor_line+3>t.line_count){
+        temp1=t.cursor_line+3-t.line_count;
     }
     else{
-        (*tail)->next=new1;
-        new1->prev=*tail;
-        *tail=new1;
+        temp1=0;
     }
+    while(temp1>0){
+        std::cout<<"\033[36m"<<'~'<<std::endl;
+        temp1--;
+    }
+    std::cout<<"\033[36m"<<std::setw(col-20)<<"Ln "<<t.cursor_line+1<<", Col "<<t.cursor_col+1<<", ";
+    if(offset==0){
+        std::cout<<"TOP";
+    }
+    else if(t.line_count<row-1||t.cursor_line+3>t.line_count){
+        std::cout<<"BOT";
+    }
+    else{
+        std::cout<<(t.cursor_line*100)/t.line_count<<"%";
+    }
+    std::cout<<"\033[0m"<<'\r';
+    t.line_offset=offset;
 }
--- display.h
+++ display.h
@@ -4,8 +4,4 @@
 #include "main.h"
 void display(unsigned short int row,unsigned short int col,text& t);
 
-void add_data(char* name,struct charn **head,struct charn **tail);
-
-void insert_last(char data,struct charn **head,struct charn **tail);
-
 #endif
