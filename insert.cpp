8e6290cb314aca166bd9f0c9b396cf16be1981d9
jagadeep017 <gantajagadeep007@gmail.com>
2025-01-19 22:17:28 +0530

    Enhance insert_before function to handle edge cases for line insertion and memory allocation checks.

--- insert.cpp
+++ insert.cpp
@@ -5,21 +5,44 @@ void insert_before(char data,text& t){
 
     if(data=='\n'){
         struct line *new1=new line;
-        new1->next=t.prev->next->next;new1->prev=t.prev->next;
         struct charn *new2=new charn;
+        if(new1==NULL||new2==NULL) {
+            std::cout<<"Memory allocation failure"<<std::endl;
+            return;
+        }
         new2->next=NULL;
         new2->data=data;
-        t.Cursor->prev->next=new2;
-        new2->prev=t.Cursor->prev;
-        new1->tail=t.prev->next->tail;
-        t.prev->next->tail=new2;
-        t.prev->next->next=new1;
-        t.Cursor->prev=NULL;
-        new1->head=t.Cursor;
+        if(t.prev){
+            new1->next=t.prev->next->next;new1->prev=t.prev->next;
+            new2->prev=t.Cursor->prev;
+            if(t.Cursor->prev){
+                t.Cursor->prev->next=new2;
+                t.Cursor->prev=NULL;
+            }
+            else{
+                t.prev->next->head=new2;
+            }
+            new1->tail=t.prev->next->tail;
+            t.prev->next->tail=new2;
+            t.prev->next->next=new1;
+            new1->head=t.Cursor;
+            t.prev_line_len=t.cursor_col=0;
+            t.prev=t.prev->next;
+        }
+        else{
+            new1->next=t.head;
+            t.head=new1;
+            new1->prev=NULL;
+            if(t.tail==NULL){
+                t.tail=new1;
+            }
+            new2->prev=NULL;
+            new1->head=new1->tail=new2;
+            t.prev=t.head;
+            t.prev->next->prev=t.head;
+        }
         t.cursor_line++;
         t.line_count++;
-        t.prev_line_len=t.cursor_col=0;
-        t.prev=t.prev->next;
     }
     else{
         std::cout<<std::endl;
